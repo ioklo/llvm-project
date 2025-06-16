7cdbf1079c987cdb65bf127f1566bb985d27b60b
IOKLO <leesjung@gmail.com>
2025-06-16 15:05:23 +0900

    correct filename in the comments

--- llvm/include/llvm/MC/MCConfig.h
+++ llvm/include/llvm/MC/MCConfig.h
@@ -1,4 +1,4 @@
-﻿//===- BinaryFormatConfig.h - Compiler abstraction support ---*- C++ -*----===//
+﻿//===- MCConfig.h - Compiler abstraction support ---*- C++ -*--------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
